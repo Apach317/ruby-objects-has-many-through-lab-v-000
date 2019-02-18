6b9a1e223c6e211ef18fda056f58064da031516e
Ron Lynch <lynch.ronald1@gmail.com>
2019-02-18 03:19:40 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,29 @@
+class Doctor
+  attr_accessor :name, :patient, :appointments
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(patient, date)
+    appointment = Appointment.new(date, patient, self)
+    @appointments << appointments
+    appointment
+  end
+
+  def appointments
+    @appointments.all.select {|appointment| appointment.doctor}
+  end
+
+  def patients
+    @appointments.collect {|meeting| meeting.patient}
+  end
+end
